2e3f5b3c65f07a01dd2cc4ad535116f03885f263
AtharvT <atharv.tare.03@gmail.com>
2023-04-02 04:32:57 +0530

    Added IPFS for cover-letters

--- src/pages/CoverLetter.tsx
+++ src/pages/CoverLetter.tsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import Moralis from "moralis";
+import { EvmChain } from "@moralisweb3/common-evm-utils";
 import copy from "copy-to-clipboard";
+import axios from "axios";
+import { base64 } from "ethers/lib/utils.js";
 
 function CoverLetter() {
   const { state } = useLocation();
+  // const state = {
+  //   resp: `Dear Hiring Manager,`,
+  // };
   console.log(state);
   const [copyText, setCopyText] = useState<string | null>(null);
   //   setCopyText(desc);
@@ -11,6 +18,10 @@ function CoverLetter() {
   const handleCopyText = (e: any) => {
     setCopyText(e.target.value);
   };
+
+  useEffect(() => {
+    setCopyText(state.resp);
+  }, []);
 
   const copyToClipboard = () => {
     if (copyText == null) {
@@ -20,6 +31,26 @@ function CoverLetter() {
       copy(copyText);
     }
   };
+
+  const handleSave = async () => {
+    await Moralis.start({
+      apiKey:
+        "fqsVW0qgF8uf28B3QZWleLIgTcfEGnrjmAlqd6MdAnYvfN2jP5Cqpk2gOCZ1Hutu",
+    });
+    const abi = [
+      {
+        path: "cover-letter.json",
+        content: {
+          name: `Cover Letter ${new Date()}}`,
+          description: copyText!,
+          image: "ipfs://QmQqzMTavQgT4f4T5v6PWBp7XNKtoPmC9jvn12WPT3gkSE",
+        },
+      },
+    ];
+    const resp = await Moralis.EvmApi.ipfs.uploadFolder({ abi });
+    console.log(resp.toJSON());
+  };
+
   const [isCopyEnabled, setIsCopyEnabled] = useState(true);
   return (
     <div className="relative bg-white  mx-auto py-8 lg:px-8">
@@ -44,6 +75,13 @@ function CoverLetter() {
           className="w-fit text-white bg-indigo-600 hover:bg-indigo-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Copy to clipboard
+        </button>
+        <button
+          type="button"
+          onClick={handleSave}
+          className="w-fit text-white bg-indigo-600 hover:bg-indigo-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Save Permanently
         </button>
       </div>
     </div>
