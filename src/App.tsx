614b14f10d93b91e14d505719bff9c4524030e47
AtharvT <atharv.tare.03@gmail.com>
2023-04-01 12:34:03 +0530

    Added Initial Form with File Upload Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <HomePage />
+    </>
+  );
 }
 
 export default App;
