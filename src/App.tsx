9a3ddc8086ebbc2984e1209858c7c0a80aaf406b
AtharvT <atharv.tare.03@gmail.com>
2023-04-01 17:37:51 +0530

    Fixed routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
-        <Route path="/" element={<CoverLetter />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cover-letter" element={<CoverLetter />} />
       </Routes>
     </>
   );
--- src/pages/CoverLetter.tsx
+++ src/pages/CoverLetter.tsx
@@ -14,8 +14,8 @@ function CoverLetter() {
 
   const copyToClipboard = () => {
     if (copyText == null) {
-      copy(desc);
-      setCopyText(desc);
+      copy(state.resp);
+      setCopyText(state.resp);
     } else {
       copy(copyText);
     }
@@ -27,7 +27,7 @@ function CoverLetter() {
         name="company"
         id="company"
         rows={20}
-        defaultValue={desc}
+        defaultValue={state.resp}
         onChange={handleCopyText}
         autoComplete="organization"
         className={`block w-3/4 md:w-1/2 rounded-md border-0 mx-auto py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${
@@ -48,19 +48,3 @@ function CoverLetter() {
 }
 
 export default CoverLetter;
-
-let desc = `Dear Hiring Manager,
-
-I am excited to submit my application for the position of Mobile Developer in your esteemed company. As an experienced developer with expertise in algorithms, programming, and mobile app development, I believe I can make a great contribution to your team.
-
-With my previous work experience at Appyhigh Technologies, I have developed apps from scratch, some of which have over 50M+ downloads in Play Store. My project "ShareKaro India" has received critical acclaim for its interactive UI and seamless transactions. My experience in cryptography adds another layer of security to the apps I develop.
-
-Apart from my professional experience, I have developed multiple personal projects that showcase my skills in Android development, Flutter, and C++ programming. I have developed an app called "FitAddict" that helps fitness enthusiasts track their calories, discover workouts, and find delicious recipes.
-
-I believe my skills, experience, and past projects make me a perfect fit for this role. I am confident that I can bring value to your organization, and I am excited to contribute to your team's success.
-
-Thank you for your time and consideration.
-
-Best regards,
-
-Atharv Tare`;
